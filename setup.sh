1f3823ce9085e45f65db05a07b10a6f3529901c6
mcserato <mcserato@up.edu.ph>
2016-03-06 18:33:12 +0800

    added comments

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-npm install
-mysql -uroot -puser < lib/CreateUser.sql
-mysql -uTeletubbies -pab1l WHOISNEXT < lib/dump.sql
+npm install #installs the nodejs dependencies
+mysql -uroot -puser < lib/CreateUser.sql #creates the user Teletubbies and the database WHOISNEXT
+mysql -uTeletubbies -pab1l WHOISNEXT < lib/dump.sql #creates the tables in the database WHOISNEXT
