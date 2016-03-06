d4009046b8755f12e2a0e31cfedd7259fde06ef2
mcserato <mcserato@up.edu.ph>
2016-03-06 18:48:19 +0800

    corrected a typo

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 npm install #installs the nodejs dependencies
 mysql -uroot -puser < lib/CreateUser.sql #creates the user Teletubbies and the database WHOISNEXT
-mysql -uTeletubbies -pab1l WHOISNEXT < lib/dump.sql #creates the tables in the database WHOISNEXT
+mysql -uTeletubbies -pab1l WHOISNEXT < lib/Dump.sql #creates the tables in the database WHOISNEXT
