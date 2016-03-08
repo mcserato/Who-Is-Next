1ae824ddb243db5ad3a4c15f4c0812fff33abf8f
alyannapauline24 <alyannapauline24@github.com>
2016-03-08 21:15:25 +0800

    [BACKEND] Edit Class

--- controllers/Class.js
+++ controllers/Class.js
@@ -5,10 +5,25 @@ exports.add = function (req, res, next) {
         + "section_number, emp_num) VALUES(?, ?, ?, ?, ?)",
         [req.body.course_code, req.body.course_title, req.body.class_section,
         req.body.section_number, req.body.emp_num],
+
         function (err, rows) {
             if (err) {
                 return(err);
             }
             res.send(rows);
 	});
+}
+
+exports.edit = function (req, res, next) {
+    db.query("UPDATE CLASS SET course_code = ?, course_title = ?, " +
+        "class_section = ?, section_number = ? WHERE class_id = ?",
+        [req.body.course_code, req.body.course_title, req.body.class_section,
+        req.body.section_number, req.body.class_id],
+
+        function (err, rows) {
+            if (err) {
+                return(err);
+            }
+            res.send(rows);
+    });
 }
