d16fb4447d3fdd87c720253e0dedf298fb85257c
alyannapauline24 <alyannapauline24@github.com>
2016-03-08 21:15:55 +0800

    [BACKEND] Edit Student

--- controllers/Student.js
+++ controllers/Student.js
@@ -1,13 +1,15 @@
 var db = require(__dirname + './../lib/Mysql');
 
 exports.add = function (req, res, next) {
-    db.query("SELECT student_number FROM STUDENT WHERE student_number=?",
+    db.query("SELECT student_number FROM STUDENT WHERE student_number = ?",
     [req.body.student_number], function(err, rows) {
         if (rows.length === 0) {
-        	db.query("INSERT INTO STUDENT VALUES (?, ?, ?, ?, ?, ?, ?, ?, STR_TO_DATE(?, '%Y-%m-%d'))",
-                [req.body.student_number, req.body.first_name, req.body.middle_name,
-                req.body.last_name, req.body.college, req.body.course,
-                req.body.gender, req.body.picture, req.body.birthday],
+        	db.query("INSERT INTO STUDENT VALUES (?, ?, ?, ?, ?, ?, ?, ?, " + 
+                "STR_TO_DATE(?, '%Y-%m-%d'))",
+                [req.body.student_number, req.body.first_name,
+                req.body.middle_name, req.body.last_name, req.body.college,
+                req.body.course, req.body.gender, req.body.picture,
+                req.body.birthday],
                 function (err, rows) {
 
                     if (err) {
@@ -19,5 +21,23 @@ exports.add = function (req, res, next) {
         else {
             res.send(400, "Error: Student already exists!");
         }
+    });
+}
+
+exports.edit = function (req, res, next) {
+    db.query("UPDATE STUDENT SET student_number = ?, first_name = ?, " +
+        "middle_name = ?, last_name = ?, college = ?, course = ?, " +
+        "gender = ?, picture = ?, birthday = STR_TO_DATE(?, '%Y-%m-%d') " +
+        "WHERE student_number = ?",
+        [req.body.student_number_new, req.body.first_name,
+        req.body.middle_name, req.body.last_name, req.body.college,
+        req.body.course, req.body.gender, req.body.picture, 
+        req.body.birthday, req.body.student_number],
+
+        function (err, rows) {
+            if (err) {
+                return(err);
+            }
+            res.send(rows);
     });
 }
