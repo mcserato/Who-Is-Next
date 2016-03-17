e8b823c16e8a90043d278c8c7a0a746ae279169e
acdeguzman <acdeguzman1@up.edu.ph>
2016-03-17 03:52:00 -0700

    Updated Analytics.js

--- controllers/Analytics.js
+++ controllers/Analytics.js
@@ -1,6 +1,6 @@
 var db = require(__dirname + './../lib/Mysql');
 
-/* Gets the top ten most called student in a specific class*/
+/* Gets the top ten most called student in a specific class */
 exports.getTopTenMostCalledStudents = function (req, res, next) {
     db.query("SELECT * FROM CLASS_STUDENT, STUDENT WHERE " +
         "CLASS_STUDENT.class_id = ? and CLASS_STUDENT.student_number = " +
@@ -8,6 +8,22 @@ exports.getTopTenMostCalledStudents = function (req, res, next) {
         "DESC LIMIT 10",
         [req.params.class_id],
         
+        function (err, rows) {
+            if (err) {
+                return next(err);
+            }
+
+            res.send(rows);
+    });
+}
+
+/* Gets the top ten most called males in a given class */
+exports.getTopTenMostCalledMales = function (req, res, next) {
+    db.query("SELECT * FROM CLASS_STUDENT,STUDENT where gender = " +
+        "'M' and CLASS_STUDENT.student_number = STUDENT.student_number and "+
+        "class_id  = ? order by no_of_times_called desc limit 10;",
+        [req.params.class_id],
+
         function (err, rows) {
             if (err) {
                 return next(err);
@@ -32,3 +48,4 @@ exports.getGenderFrequency = function (req, res, next) {
             res.send(rows);
     });
 }
+
