77e6c08d65cd769981b639fdcee9edc9705772aa
pericodan <perico.dionisio@gmail.com>
2016-03-17 17:59:02 +0800

    [BACKEND] Added get top ten most called students per class for analytics

--- controllers/Analytics.js
+++ controllers/Analytics.js
@@ -1,1 +1,16 @@
 var db = require(__dirname + './../lib/Mysql');
+
+/* Gets the top ten most called student in a specific class*/
+exports.getTopTenMostCalledStudents = function (req, res, next) {
+    db.query("SELECT * FROM CLASS_STUDENT, STUDENT WHERE " +
+        "CLASS_STUDENT.class_id = ? and CLASS_STUDENT.student_number = " +
+        "STUDENT.student_number ORDER BY CLASS_STUDENT.no_of_times_called " +
+        "DESC LIMIT 10;",
+        [req.params.class_id],
+        function (err, rows) {
+            if (err) {
+                return next(err);
+            }
+            res.send(rows);
+    });
+}
