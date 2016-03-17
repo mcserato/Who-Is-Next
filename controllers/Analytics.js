2cf9b6927f38076714805c2dd6ad3fb726ed8526
pericodan <perico.dionisio@gmail.com>
2016-03-17 19:20:03 +0800

    Updated Analytics.js

--- controllers/Analytics.js
+++ controllers/Analytics.js
@@ -8,7 +8,7 @@ exports.getTopTenMostCalledStudents = function (req, res, next) {
         "STUDENT.student_number ORDER BY CLASS_STUDENT.no_of_times_called " +
         "DESC LIMIT 10",
         [req.params.class_id],
-        
+
         function (err, rows) {
             if (err) {
                 return next(err);
@@ -20,8 +20,8 @@ exports.getTopTenMostCalledStudents = function (req, res, next) {
 
 /*Gets the section frequency in a specific class*/
 exports.getSectionFrequency = function (req, res, next) {
-    db.query("SELECT SUM(CLASS_STUDENT.no_of_times_called) FROM " + 
-        "CLASS_STUDENT, STUDENT WHERE CLASS_STUDENT.student_number = " + 
+    db.query("SELECT SUM(CLASS_STUDENT.no_of_times_called) as frequency FROM " +
+        "CLASS_STUDENT, STUDENT WHERE CLASS_STUDENT.student_number = " +
         "STUDENT.student_number AND CLASS_STUDENT.class_id = ? AND " +
         "STUDENT.student_number IN (SELECT STUDENT.student_number FROM " +
         "CLASS_STUDENT, STUDENT WHERE CLASS_STUDENT.student_number = " +
@@ -59,10 +59,18 @@ exports.getTopTenMostCalledFemales = function (req, res, next) {
         "'F' and CLASS_STUDENT.student_number = STUDENT.student_number and "+
         "class_id  = ? order by no_of_times_called desc limit 10;",
         [req.params.class_id],
+        function (err, rows) {
+            if (err) {
+                return next(err);
+            }
+
+            res.send(rows);
+    });
+}
 
 /* Gets the gender frequency in a specific class */
 exports.getGenderFrequency = function (req, res, next) {
-    db.query("SELECT  SUM(no_of_times_called) FROM STUDENT, CLASS_STUDENT " +
+    db.query("SELECT  SUM(no_of_times_called) as frequency FROM STUDENT, CLASS_STUDENT " +
         "WHERE STUDENT.student_number = CLASS_STUDENT.student_number and " +
         "gender = ? AND class_id = ?",
         [req.params.gender, req.params.class_id],
@@ -75,4 +83,3 @@ exports.getGenderFrequency = function (req, res, next) {
             res.send(rows);
     });
 }
-
