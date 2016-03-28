5dd44368676f9154f33f928c42c6bdf704fb81ef
rlbayeta2589 <usefakename@gmail.com>
2016-03-28 20:52:42 +0800

    handle case when localStorage.user is not yet define

--- public/script/Config.js
+++ public/script/Config.js
@@ -2,9 +2,9 @@ var config = {
     TITLE: 'Who Is Next! - A Classroom Selector of Volunteers',
     FRONTEND_URL: 'localhost:8000/',
     checkAuth: function(expected_role){
-    	var user = JSON.parse(localStorage.user);
-    	if(user.role != expected_role){
+    	if(!localStorage.user ||
+    			JSON.parse(localStorage.user).role != expected_role){
 	        window.location.href = '/';
-	    }
+    	}
     }
 };
