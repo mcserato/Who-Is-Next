8cd2d8b5e7710e931db81dd23eb29fd085e20f55
rlbayeta2589 <usefakename@gmail.com>
2016-03-28 20:26:35 +0800

    Connected Login BE and FE and fixed script.js links

--- public/script/Config.js
+++ public/script/Config.js
@@ -1,5 +1,10 @@
 var config = {
     TITLE: 'Who Is Next! - A Classroom Selector of Volunteers',
-    FRONTEND_URL: 'localhost:8000/'
+    FRONTEND_URL: 'localhost:8000/',
+    checkAuth: function(expected_role){
+    	var user = JSON.parse(localStorage.user);
+    	if(user.role != expected_role){
+	        window.location.href = '/';
+	    }
+    }
 };
-
