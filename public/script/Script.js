7d4cf8b03e5e493b278397cd29351a99bf35a044
rlbayeta2589 <usefakename@gmail.com>
2016-03-28 13:05:46 +0800

    Added homepage and login frontend

--- public/script/Script.js
+++ public/script/Script.js
@@ -1,8 +1,41 @@
 'use strict';
 
 $(document).ready( function () {
-	document.title += document.title
-		? ' - ' + config.TITLE
-		: config.TITLE;
+    document.title += document.title
+        ? ' - ' + config.TITLE
+        : config.TITLE;
 
+    $('.modal-trigger').leanModal();
+
+    $('#logo')
+        .hover(hoverIn,hoverOut);
+
+    $('#sign-up')
+        .hover(hoverIn,hoverOut);
+
+    $('#log-in')
+        .hover(hoverIn,hoverOut);
+
+    function hoverIn(){
+        $('#logo').attr("src", '/icon/logo1.gif');
+    }
+    function hoverOut(){
+        $('#logo').attr("src", '/icon/logo1.png');
+    }
+
+    $('#login')
+        .click(function(){
+            var username = $('#username').val(),
+                password = $('#password').val();
+
+            if(!username){
+                return Materialize.toast("Username is missing !", 2500);
+            }
+            if(!password){
+                return Materialize.toast("Password is missing !", 2500);
+            }
+
+            // ajax call here
+            
+        });
 });
