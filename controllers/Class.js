3375e076115729cc72721c325a66dfa6d8897f0f
Rokuto <gauven.boro@gmail.com>
2016-04-05 14:56:22 +0800

    Rollback

--- controllers/Class.js
+++ controllers/Class.js
@@ -93,16 +93,13 @@ exports.viewAll = function(req, res, next) {
 exports.viewArchived = function(req, res, next) {
     db.query("SELECT * FROM CLASS where emp_num = ? and is_archived = 1", [req.params.emp_num], function (err, rows) {
 		if (err) {
-		    //return next(err);
-            res.render('400');
+		    return next(err);
 		}
 		
 		if (rows.length === 0) {
-		    //res.send(404, "Error: Classes not found.");
-            res.render('404');
+		    res.send(404, "Error: Classes not found.");
 		} else {
-			//res.send(rows);
-            res.render('Classes', {classes:rows});
+			res.send(rows);
 		}
     });
 }
@@ -116,11 +113,9 @@ exports.viewOne = function(req, res, next) {
 		    }
 		
 		    if (rows.length === 0) {
-		        //res.send(404, "Error: Class not found.");
-                res.render('404');
+		        res.send(404, "Error: Class not found.");
 		    } else {
-			    //res.send(rows);
-                res.render('Class', {class:rows[0]});
+			    res.send(rows);
 		    }
     });
 }
@@ -134,11 +129,9 @@ exports.search = function(req, res, next) {
             }
         
             if (rows.length === 0) {
-                //res.send(404, "Error: Class not found.");
-                res.render('404');
+                res.send(404, "Error: Class not found.");
             } else {
-                //res.send(rows);
-                res.render('Classes', {classes:rows});
+                res.send(rows);
             }
 	});
 }
