e76ead8abd3857678ecbc49048203f2bb91ff4b8
Rokuto <gauven.boro@gmail.com>
2016-04-20 07:28:54 -0400

    CSV

---
+++ public/views/csv/Script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+$(document).ready( function () {
+        
+    $('.modal-trigger')
+        .leanModal({
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .5, // Opacity of modal background
+            in_duration: 300, // Transition in duration
+            out_duration: 200, // Transition out duration
+            ready: function() { }, // Callback for Modal open
+            complete: function() { } // Callback for Modal close
+        }
+    );
+
+
+    console.log($('#file-path').val());
+    
+
+
+});
