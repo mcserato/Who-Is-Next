f74ae13e8b301a41cdc1fab5da8ff77150560375
rlbayeta2589 <usefakename@gmail.com>
2016-05-02 10:28:22 +0800

    added test feature activate faculty after registration

--- public/not_validated/Script.js
+++ public/not_validated/Script.js
@@ -5,24 +5,25 @@ $(document).ready( function () {
     $('#validate-btn')
         .click(function(){
 
-            $.ajax({
-                url: '/api/validate',
-                method: 'POST',
-                success: function(data){
-                    if(!data){
-                        return Materialize.toast("Error in Validation. Please try again !",2500);
+            if(confirm("This is only a test feature. Would you like to continue ?")){
+                $.ajax({
+                    url: '/api/validate',
+                    method: 'POST',
+                    success: function(data){
+                        if(!data){
+                            return Materialize.toast("Error in Validation. Please try again !",2500);
+                        }
+
+                        return Materialize.toast("Successfully Validated!",1000,"",function(){
+                            window.location.href = "/views/class";
+                        });
+                        
+                    },
+                    error: function(err){
+                        return Materialize.toast(err.responseText,2500);
                     }
-
-                    return Materialize.toast("Successfully Validated!",1000,"",function(){
-                        window.location.href = "/views/class";
-                    });
-                    
-                },
-                error: function(err){
-                    return Materialize.toast(err.responseText,2500);
-                }
-            });
-
+                });
+            }
         });
 
 
