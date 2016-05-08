9f1002c2bbc110665ffa0f3041dadd7ffd3a4582
rlbayeta2589 <usefakename@gmail.com>
2016-05-09 02:56:08 +0800

    frontend url test

--- public/script/Config.js
+++ public/script/Config.js
@@ -1,6 +1,6 @@
 var config = {
     TITLE: 'Who Is Next! - A Classroom Selector of Volunteers',
-    FRONTEND_URL: 'http://localhost:8000/',
+    FRONTEND_URL: process.env.OPENSHIFT_FRONTEND_URL || 'http://localhost:8000/',
     checkAuth: function(expected_role){
     	if(!localStorage.user ||
     			JSON.parse(localStorage.user).role != expected_role){
