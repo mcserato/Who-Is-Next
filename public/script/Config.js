7082e8da3f97e2a25255c19ea4dc37aed66a1a8d
systemplado <systemplado@gmail.com>
2016-05-17 05:16:57 +0800

    updated config.checkAuth()

--- public/script/Config.js
+++ public/script/Config.js
@@ -2,9 +2,18 @@ var config = {
     TITLE: 'Who Is Next! - A Classroom Selector of Volunteers',
     FRONTEND_URL: 'http://localhost:8000/',
     checkAuth: function(expected_role){
-    	if(!localStorage.user ||
-    			JSON.parse(localStorage.user).role != expected_role){
-	        window.location.href = '/';
+        var user = JSON.parse(localStorage.user);
+
+        if(!expected_role) {
+            if (user.role !== ('ADMIN' || 'FACULTY')) {
+                return window.location.href = '/';
+            }
+
+            return;
+        }
+
+    	if(!user || user.role != expected_role){
+	        return window.location.href = '/';
     	}
     }
 };
